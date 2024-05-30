3508cd1cb85c90f6269aa35d6e12838244a0fd8e
Skarginson <eldaraes@gmail.com>
2024-05-30 09:52:04 +0200

    Changes to mail regex

--- consts.js
+++ consts.js
@@ -7,7 +7,8 @@ const PORT = process.env.PORT || 3000;
 const TOKEN_SECRET = process.env.TOKEN_SECRET;
 
 const passwordRegex = /^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/;
-const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const emailRegex =
+  /^(?=.{5,254}$)(?=.{1,64}@)(([a-z]|\d)+[-_+\.]?)*([a-z]|\d)@(?=.{3,252}$)(?=.{1,63}\.)(([a-z]|\d)+-?)*([a-z]|\d)\.(?=.{1,63}$)(([a-z]+-?)*([a-z]|\d))$/g;
 
 module.exports = {
   MONGO_URI,
