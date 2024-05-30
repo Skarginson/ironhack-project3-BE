2efc9166c15165352b7219e0d3e23ac86c25c8ce
Skarginson <eldaraes@gmail.com>
2024-05-30 14:12:34 +0200

    fixed cors required

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const logger = require("morgan");
+const cors = require("cors");
 
 const { PORT } = require("./consts");
 const usersRouter = require("./src/routes/users.router");
