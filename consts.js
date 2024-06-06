20751be2307d9b5708c67d0f3687fe89c09804db
Skarginson <eldaraes@gmail.com>
2024-06-06 15:45:45 +0200

    setup cors + secured routes

--- consts.js
+++ consts.js
@@ -6,6 +6,8 @@ const PORT = process.env.PORT || 3000;
 
 const TOKEN_SECRET = process.env.TOKEN_SECRET;
 
+const CORS_ORIGIN = process.env.CORS_ORIGIN || "http://localhost:5173";
+
 const passwordRegex = /^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/;
 const emailRegex =
   /^(?=.{5,254}$)(?=.{1,64}@)(([a-z]|\d)+[-_+\.]?)*([a-z]|\d)@(?=.{3,252}$)(?=.{1,63}\.)(([a-z]|\d)+-?)*([a-z]|\d)\.(?=.{1,63}$)(([a-z]+-?)*([a-z]|\d))$/g;
@@ -15,5 +17,6 @@ module.exports = {
   PORT,
   TOKEN_SECRET,
   passwordRegex,
+  CORS_ORIGIN,
   emailRegex,
 };
